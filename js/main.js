3082d7f71d43b25a7ff6bd1be1ee74348c796a37
whiteli <white.li@6edigital.com>
2019-05-24 16:35:12 +0800

    fix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 window.onload = function () {
     progressively.init()
+    $(".header-btn-close").click(function() {
+        console.log('close');
+        $(".header-overlay").removeClass("overlay-opened");
+    })
+    $(".header-btn-dropdown").click(function() {
+        $(".header-overlay").addClass("overlay-opened");
+    })
+    $(".header-btn-close a").click(function() {
+        $(".header-overlay").removeClass("overlay-opened");
+    })
 }
